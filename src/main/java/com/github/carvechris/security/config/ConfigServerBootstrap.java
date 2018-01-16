976310a9008f039479c3df3b75547fa4365202b5
carvechris <carvechris@126.com>
2018-01-16 12:03:15 +0800

    config update

--- src/main/java/com/github/carvechris/security/config/ConfigServerBootstrap.java
+++ src/main/java/com/github/carvechris/security/config/ConfigServerBootstrap.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
- * Created by ace on 2017/7/29.
+ * Created by carvechris on 2017/7/29.
  */
 @EnableAutoConfiguration
 @EnableEurekaClient
